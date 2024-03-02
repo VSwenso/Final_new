89a26afc0bb26408017bf2819256c58a6aa0f00f
“Kaylie <klepley@ucsc.edu>
2024-03-01 22:19:43 -0800

    UPLR Movements

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -2,63 +2,53 @@ class Level1 extends Phaser.Scene {
     constructor() {
         super('Level1');
     }
-    
+
     init() {
-        this.PLAYER_VELOCITY = 350
+        this.PLAYER_VELOCITY = 350;
     }
 
     preload() {
-
-        
-        //Load background
-        this.load.image('runnerback', './assets/runnerback.png')
-
-        //Load kid
-        this.load.image('kidskate', './assets/KIDskateboard.png')
-
-    
+        // Load background
+        this.load.image('runnerback', './assets/runnerback.png');
+        // Load kid
+        this.load.image('kidskate', './assets/KIDskateboard.png');
     }
-    create() {
 
+    create() {
         this.runnerback = this.add.tileSprite(0, 0, 800, 600, 'runnerback').setOrigin(0, 0);
 
-}
+        // Add the kid sprite and set its initial position
+        this.kidskate = this.physics.add.sprite(100, 300, 'kidskate').setOrigin(0.5, 0.5);
 
-   update() {
+        // Enable physics for the kid sprite
+        this.physics.world.enable(this.kidskate);
 
+        // Set the kid sprite to collide with the world bounds
+        this.kidskate.setCollideWorldBounds(true);
+    }
+
+    update() {
         // Continuously scroll the background to the left
         this.runnerback.tilePositionX += 4; // Adjust the scrolling speed as needed
 
+        // Get keyboard input
+        const cursors = this.input.keyboard.createCursorKeys();
+
+        // Update player movement based on arrow keys
+        if (cursors.left.isDown) {
+            this.kidskate.setVelocityX(-this.PLAYER_VELOCITY);
+        } else if (cursors.right.isDown) {
+            this.kidskate.setVelocityX(this.PLAYER_VELOCITY);
+        } else {
+            this.kidskate.setVelocityX(0);
+        }
+
+        if (cursors.up.isDown) {
+            this.kidskate.setVelocityY(-this.PLAYER_VELOCITY);
+        } else if (cursors.down.isDown) {
+            this.kidskate.setVelocityY(this.PLAYER_VELOCITY);
+        } else {
+            this.kidskate.setVelocityY(0);
+        }
     }
 }
-
-//     gameOver() {
-//         // Custom game over logic
-//         const deathSound = this.sound.add('death');
-//         deathSound.play();        // You can add any game over logic here, such as displaying a game over message or restarting the game.
-//         this.scene.start('GameOver');
-//         // Add your custom game over logic here
-//     }
-
-//     speedUpEverything() {
-//         // Increase the velocity of the player
-//         this.PLAYER_VELOCITY *= 1.2;  // You can adjust the factor as needed
-    
-//         // Increase the velocity of enemies
-//         this.enemies.children.iterate((enemy) => {
-//             enemy.body.velocity.x *= 1.2;  // You can adjust the factor as needed
-//         });
-    
-//         // Increase the velocity of seaweeds
-//         this.topSeaweeds.children.iterate((seaweed) => {
-//             seaweed.body.velocity.x *= 1.2;  // You can adjust the factor as needed
-//         });
-    
-//         this.bottomSeaweeds.children.iterate((seaweed) => {
-//             seaweed.body.velocity.x *= 1.2;  // You can adjust the factor as needed
-//         });
-    
-//         // You can add other elements that you want to speed up
-//     }
-
-// }
