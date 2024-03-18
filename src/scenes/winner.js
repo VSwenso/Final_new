0ce18b8dd0a764edf5dcad5056fb00a757448860
VSwenso <129703809+VSwenso@users.noreply.github.com>
2024-03-18 14:55:01 -0700

    now fix

--- src/scenes/winner.js
+++ src/scenes/winner.js
@@ -46,6 +46,7 @@ class winner extends Phaser.Scene {
         };
 
         // Animation from 'allsprites' sprite sheet
+        let allSprites = this.physics.add.sprite(100, this.scale.height / 2);
         allSprites.setVelocityX(44); // Set initial velocity along X-axis
 
 
